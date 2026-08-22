c34c0af9a99460ab185badb5df4df378f40fb204
mathgirl314 <jam.t.le.3.14@gmail.com>
2026-08-22 00:15:13 +0000

    Deploying to gh-pages from @ mathgirl314/mathgirl314.github.io@19d93e9ae63c6e5553ac70b5adb29d1f54fb305e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-jamie-t-le",
+    title: "Jamie T. Le",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
